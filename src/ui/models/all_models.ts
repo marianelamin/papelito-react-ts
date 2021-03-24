21346a6903d5f44a303098a76d4fc00fe6b0884a
Marianela Crissman <marianela_min@yahoo.com>
2021-03-23 21:16:44 -0500

    group main classes for future imports

---
+++ src/ui/models/all_models.ts
@@ -0,0 +1,6 @@
+import Papelito from 'ui/models/papelito'
+import Player from 'ui/models/player'
+import Team from 'ui/models/team'
+import Room from 'ui/models/room'
+
+export { Papelito, Player, Team, Room }
